e34bf77fc049a6906b681517cdc4a7844bc4f5f4
unknown <nur_han98@mail.ru>
2018-12-20 01:25:23 +0600

    Zdes registration

--- authentication/views.py
+++ authentication/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
-from django.conf import settings
-# Create your views here.
+from django.contrib.auth import authenticate
+from .models import User
 
 def register(request):
     if request.method == "POST":
-        print(request.POST)
+        try:
+            user = User.objects.create_user(
+                email=request.POST['email'],
+                password=request.POST['password'],
+            )
+            user.first_name = request.POST['first_name']
+            user.last_name = request.POST['last_name']
+            user.save()
+            return print(request.user)
+        except:
+            print("ERROR")
     else:
         return render(request, 'registration/registration.html')
