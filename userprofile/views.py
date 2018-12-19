324f5ab3694ff284e31c98445083b0ae5229b35d
unknown <nur_han98@mail.ru>
2018-12-20 04:02:55 +0600

    edit

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from cities.models import City
-
+from .models import Profile
 # Create your views here.
 def profileEdit(request):
     if request.method == 'POST':
-        return print(request.POST)
+        p = Profile()
+        p.user = request.user
+        p.city = City.objects.get(pk=request.POST['city'])
+        p.phone = request.POST['phone']
+        p.skype = request.POST['skype']
+        p.date_of_birth = request.POST['date_of_birth']
+        p.image = request.POST['image']
+        p.save()
     else:
         cities = City.objects.all()
         return render(request, 'profile/edit.html', {'cities': cities})
