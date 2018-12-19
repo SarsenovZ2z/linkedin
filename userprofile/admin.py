8f3050b2937028cb86970704867bedb72864e3cc
unknown <nur_han98@mail.ru>
2018-12-19 17:22:28 +0600

    Minani men jasadim

--- userprofile/admin.py
+++ userprofile/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import *
 
+class User_profileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'city')
 
-admin.site.register(User_profile)
+
+admin.site.register(User_profile, User_profileAdmin)
--- vacanciesandsvs/admin.py
+++ vacanciesandsvs/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class EducationAdmin(admin.ModelAdmin):
+    pass
+    # list_display = ('name', 'code')
+
+admin.site.register(Education, EducationAdmin)
